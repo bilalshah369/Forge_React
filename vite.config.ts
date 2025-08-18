06edd030d0794192f82dde1b96a710c50b8cddea
bilalshah369 <bilalshah369@yahoo.com>
2025-08-18 11:54:55 +0530

    icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
-import svgr from 'vite-plugin-svgr';
+import svgr from "vite-plugin-svgr";
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
@@ -10,13 +11,36 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [
-    react(), svgr(),
-    mode === 'development' &&
-    componentTagger()
-  ].filter(Boolean),
+    react(),
+    svgr()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 }));
+
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import path from "path";
+// import { componentTagger } from "lovable-tagger";
+// import svgr from 'vite-plugin-svgr';
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   server: {
+//     host: "::",
+//     port: 8080,
+//   },
+//   plugins: [
+//     react(), svgr(),
+//     mode === 'development' &&
+//     componentTagger()
+//   ].filter(Boolean),
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
